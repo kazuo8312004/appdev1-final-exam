2b5acb9f2f290ce0d6f33fbce398e0bb6f607df1
kazuo8312004 <delpilarkirk@gmail.com>
2025-12-07 19:09:23 +0800

    Implement Home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Todo App</h1>
+      <nav>
+        <Link to="/login">Login</Link> | <Link to="/todos">Todos</Link>
+      </nav>
+    </main>
+  );
+}
